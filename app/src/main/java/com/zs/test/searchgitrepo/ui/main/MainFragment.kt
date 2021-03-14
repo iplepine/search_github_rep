2fcac6808394dfac379238dccdf5a4578bcd2c40
DESKTOP-A2TR5DO\Jungho <luckjungho@gmail.com>
2021-03-14 12:19:28 +0900

    databinding 추가, MainFragment UI 기본 구성

--- app/src/main/java/com/zs/test/searchgitrepo/ui/main/MainFragment.kt
+++ app/src/main/java/com/zs/test/searchgitrepo/ui/main/MainFragment.kt
@@ -1,12 +1,12 @@
 package com.zs.test.searchgitrepo.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.zs.test.searchgitrepo.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.zs.test.searchgitrepo.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,16 +15,21 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = MainFragmentBinding.inflate(inflater)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewModel = viewModel
     }
 
 }

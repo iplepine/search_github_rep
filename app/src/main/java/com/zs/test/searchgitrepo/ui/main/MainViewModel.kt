84dd9b1e211993c226dcc3c65283842533969e6f
DESKTOP-A2TR5DO\Jungho <luckjungho@gmail.com>
2021-03-14 13:07:33 +0900

    MainViewModel에 일부 기능 추가

--- app/src/main/java/com/zs/test/searchgitrepo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/zs/test/searchgitrepo/ui/main/MainViewModel.kt
@@ -1,7 +1,63 @@
 package com.zs.test.searchgitrepo.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zs.test.searchgitrepo.R
+import com.zs.test.searchgitrepo.data.ResourceHelper
+import com.zs.test.searchgitrepo.data.network.RetrofitManager
+import com.zs.test.searchgitrepo.data.network.api.GitService
+import com.zs.test.searchgitrepo.data.network.entity.Repository
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val errorMessage = MutableLiveData<String>("검색어를 입력하세요.")
+    val query = MutableLiveData<String>("")
+    var page = 0
+    val list = ArrayList<Repository>()
+
+    val gitService by lazy {
+        RetrofitManager.createService(GitService::class.java)
+    }
+
+    private fun showErrorMessage(message: String) {
+        errorMessage.postValue(message)
+    }
+
+    fun onClickSearch() {
+        page = 0
+        list.clear()
+
+        viewModelScope.launch {
+            query.value.let {
+                if (it == null || it.isEmpty()) {
+                    showErrorMessage(ResourceHelper.getString(R.string.error_query_empty))
+                } else {
+
+                }
+            }
+        }
+    }
+
+    private fun notifyDataChanged() {
+    }
+
+    suspend fun search(isAppend: Boolean) {
+        if (isAppend) {
+            page++
+        } else {
+            page = 0
+            list.clear()
+            notifyDataChanged()
+        }
+
+        query.value?.also {
+            val result = gitService.getRepos(it, page)
+        }
+    }
+
+    fun onSuccessResult() {
+
+    }
 }
